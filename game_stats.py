ac8954fe873264af504b75de6ea8817e01b74c61
AlmasShintemirov <ashintemirov@gmail.com>
2022-05-28 19:01:33 +0600

    Basic game

---
+++ game_stats.py
@@ -0,0 +1,13 @@
+class GameStats:
+    """track statistics for alien invasion"""
+
+    def __init__(self, ai_game):
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+        #start alien invasion in an inactive state
+        self.game_active = False
+
+
+    def reset_stats(self):
+        self.ships_left = self.settings.ship_limit
